47b4d0e5ffb5a2182f35d3073be2f15ff089dbf5
Laura Gjoni <Laura_Gjoni@sluz.ch>
2024-09-19 10:55:32 +0200

    formatted

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,13 @@
-const config =
-  {
-    rules:
-      {
-        eqeqeq:
-          'error',
-        'no-unused-vars':
-          'error',
-      },
-    env: {
-      node: true,
-      es6: true,
-    },
-    extends:
-      'eslint:recommended',
-  };
+const config = {
+  rules: {
+    eqeqeq: 'error',
+    'no-unused-vars': 'error',
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: 'eslint:recommended',
+};
 
-module.exports =
-  config;
+module.exports = config;
--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 const chalk = require('chalk');
-console.log(
-  chalk.blue(
-    'It works',
-  ),
-);
+console.log(chalk.blue('It works'));
