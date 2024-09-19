6eff9ba413b827805914a3fc23f4cae2f4ea1df3
Laura Gjoni <Laura_Gjoni@sluz.ch>
2024-09-19 11:07:17 +0200

    errors fixed

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ console.log(chalk.blue('It works'));
   export function calculate(a, b, operation) {
     switch (operation) {
       case 'add':
-        return a - b;
-      case 'subtract':
         return a + b;
+      case 'subtract':
+        return a - b;
       default:
         throw new Error('Invalid operation');
     }
