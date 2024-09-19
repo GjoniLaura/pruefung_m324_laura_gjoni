fa90f0e0874e24ed69d14412228721d2b60f7993
Laura Gjoni <Laura_Gjoni@sluz.ch>
2024-09-19 10:49:58 +0200

    linter test

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+const config = {
+    rules: {
+      eqeqeq: 'error',  
+      'no-unused-vars': 'error', 
+    },
+    env: {
+      node: true,
+      es6: true,  
+    },
+    extends: "eslint:recommended", 
+  };
+  
+  module.exports = config;
+  
